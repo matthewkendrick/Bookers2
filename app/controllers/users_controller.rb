0a645cd1c19f2305bc484c6073aa0a50b140934e
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-02-06 10:10:39 +0000

    [Fix]: Adusting the Routing
    Message body
    Adusting the routing after sign-in.
    
    Message footer
    Application2_Assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,47 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_patams)
+    if @user.save
+      redirect_to user_path, success: 'Welcome! You have signed up successfully.'
+    else
+      render :create
+    end
+  end
+
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def index
+    @users = User.all
+    @book = Book.new
+    @user = current_user
   end
 
   def update
+    @user = User.find(params[:id])
+    @books = Book.all
+    if @user.update(user_arams)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to current_user
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
