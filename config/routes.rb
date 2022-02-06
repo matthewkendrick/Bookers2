d27474407084efa9ad979409ac731c70068ee6fd
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-02-06 03:57:41 +0000

    [Edit]: Make it looks good for top page
    Message body
    Edit top.html.erb for top page, and route has updated too
    
    Message footer
    Application2_Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/index'
-  get 'users/update'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/create'
-  get 'books/show'
-  get 'books/index'
-  get 'books/update'
-  get 'books/edit'
-  get 'books/destroy'
-  get 'homes/top'
-  get 'homes/about'
+
   devise_for :users
+  root to: "homes#top"
+  get 'homes/about' => 'homes#about'
+
+  resources :books
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
