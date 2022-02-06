21d3dc9d276a39a69427b77b6086015da630a9e9
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-02-06 00:04:38 +0000

    [Add]: Install the Bootstrap and the FontAwesome
    Message body
    Install Bootstrap and FontAwesome that prepare to look nice for.
    
    Message footer
    Application2_Assignment

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
+  get 'users/update'
+  get 'users/edit'
+  get 'books/new'
+  get 'books/create'
+  get 'books/show'
+  get 'books/index'
+  get 'books/update'
+  get 'books/edit'
+  get 'books/destroy'
+  get 'homes/top'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
