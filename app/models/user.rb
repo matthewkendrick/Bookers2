cf4ba770349cec43c3482df8dad9b762cd7e8ea5
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-02-06 21:29:03 +0000

    [Edit]: Created a page about user info
    Message body
    However, partial templating wasn't chieved.
    
    Message footer
    Application2_Assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
+  has_one_attached :profile_image
+
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
