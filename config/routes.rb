ba28ff036ab190366c37b70380c135e9eba3217a
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-02-08 21:38:28 +0000

    [Add]: Validations
    Message body
    Add validation for failure about when it's new posts, updating posts, and updating profile
    
    Message footer
    Application2_Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about' => 'homes#about'
 
-  resources :books
-  resources :users
+  resources :books, only: [:show, :edit, :create, :index, :destroy, :update]
+  resources :users, only: [:show, :create, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
